94a2c85e9033cf807f9c8b07b112ff8fdd8d6b02
yndvp <yunna3828@gmail.com>
2021-02-23 21:39:59 +0900

    .

--- script.js
+++ script.js
@@ -8,41 +8,38 @@ const youChose = document.querySelector(".youChose");
 const result = document.querySelector(".result");
 
 function getRandomNumberBetween(min, max) {
-    const small = Math.ceil(min);
-    const big = Math.floor(max);
-    return Math.floor(Math.random()*(big-small+1))+small;
+  const small = Math.ceil(min);
+  const big = Math.floor(max);
+  return Math.floor(Math.random() * (big - small + 1)) + small;
 }
 
 function didOrNot(event) {
-    event.preventDefault();
-    if(input.value !== "") {
-        handleSubmit();
-    } 
+  event.preventDefault();
+  if (input.value !== "") {
+    handleSubmit();
+  }
 }
 
 function handleSubmit() {
-    const rangeValue = range.value;
-    const inputValue = input.value;
-    const randomValue = getRandomNumberBetween(0, rangeValue);
-    youChose.innerHTML = `You chose: ${inputValue}, the machine chose: ${randomValue}`;
-    if (parseInt(inputValue) === randomValue) {
-        result.innerHTML = "You won!🎊";
-        document.querySelector('body').style.backgroundColor="#efff64";
-
-    } else {
-        result.innerHTML = "You lost😥";
-        document.querySelector('body').style.backgroundColor="#5e4f4f";
-
-    }
+  const rangeValue = range.value;
+  const inputValue = input.value;
+  const randomValue = getRandomNumberBetween(0, rangeValue);
+  youChose.innerHTML = `You chose: ${inputValue}, the machine chose: ${randomValue}`;
+  result.style.display = "block";
+  youChose.style.display = "block";
+  if (parseInt(inputValue) === randomValue) {
+    result.innerHTML = "You won!🎊";
+    document.querySelector("body").style.backgroundColor = "#E2E1E0";
+  } else {
+    result.innerHTML = "You lost😥";
+    document.querySelector("body").style.backgroundColor = "#525252";
+  }
 }
 
 function loadNumber() {
-    const rangeValue = range.value;
-    generate.innerHTML = `Generate a number between 0 and ${rangeValue}`;
-
+  const rangeValue = range.value;
+  generate.innerHTML = `Generate a number between 0 and ${rangeValue}`;
 }
 
-range.addEventListener("input",loadNumber);
+range.addEventListener("input", loadNumber);
 form.addEventListener("submit", didOrNot);
-
-
